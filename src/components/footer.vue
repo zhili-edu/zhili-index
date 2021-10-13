d825faa03901080388712411b3cfb92d4830ed72
c0per <60544162+c0per@users.noreply.github.com>
2021-10-13 15:26:53 +0800

    更新备案信息

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -33,7 +33,10 @@
 			<div class="grid justify-center pt-6 lg:justify-between">
 				<div class="flex flex-col self-center text-sm text-center md:block lg:col-start-1 md:space-x-6">
 					<span>©2021 zhili-edu. All rights reserved</span>
-          <span>京ICP备2021029169号</span>
+                    <span>京ICP备2021029169号</span>
+                    <div style="width:300px;margin:0 auto; padding:20px 0;">
+                        <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=13030202003280" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="/beian.png" style="float:left;"/><p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">冀公网安备 13030202003280号</p></a>
+                    </div>
 				</div>
 				<div class="flex justify-center pt-4 space-x-4 lg:pt-0 lg:col-end-13">
 					<a href="wahacercn@gmail.com" title="Email" class="flex items-center justify-center w-10 h-10 rounded-full bg-violet-600 text-coolGray-50">
